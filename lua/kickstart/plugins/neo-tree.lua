947702a2b0a69736a8f1c9d37e7ff3186904af6b
maarten442 <maartenrottier@live.nl>
2025-04-12 12:52:23 +0200

    initial setup

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -1,6 +1,8 @@
 -- Neo-tree is a Neovim plugin to browse the file system
 -- https://github.com/nvim-neo-tree/neo-tree.nvim
 
+-- Replace the content of lua/kickstart/plugins/neo-tree.lua with:
+
 return {
   'nvim-neo-tree/neo-tree.nvim',
   version = '*',
@@ -11,15 +13,36 @@ return {
   },
   cmd = 'Neotree',
   keys = {
-    { '\\', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
+    { '<leader>n', ':Neotree reveal<CR>', desc = 'NeoTree Explorer', silent = true },
   },
   opts = {
     filesystem = {
-      window = {
-        mappings = {
-          ['\\'] = 'close_window',
+      follow_current_file = true, -- Follow current file when opening/changing buffers
+      hijack_netrw_behavior = 'open_default', -- netrw disabled, opening a directory opens neo-tree
+      use_libuv_file_watcher = true, -- This will use the OS level file watchers to detect changes
+      filtered_items = {
+        visible = true, -- This makes hidden files visible by default
+        show_hidden_count = true,
+        hide_dotfiles = false, -- Set to false to show dotfiles
+        hide_gitignored = false, -- Set to false to show gitignored files
+        hide_by_name = {
+          -- '.git',              -- Uncomment if you want to hide the .git folder
+          -- 'node_modules',      -- Uncomment if you want to hide node_modules
+        },
+        never_show = {
+          -- '.DS_Store',         -- Uncomment if you never want to see .DS_Store
         },
       },
+      window = {
+        mappings = {
+          ['<leader>n'] = 'close_window', -- Close the tree when pressing <leader>n again
+          ['H'] = 'toggle_hidden', -- Add a mapping to easily toggle hidden files
+        },
+      },
+    },
+    window = {
+      position = 'left',
+      width = 30,
     },
   },
 }
